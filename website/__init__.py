4b8db8ce62d51269aa157bffc44d6ee639758c84
johnald1337 <johnald1337@hotmail.com>
2023-03-26 12:57:47 +0800

    Update DB

--- website/__init__.py
+++ website/__init__.py
@@ -5,14 +5,12 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'abcd'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
-
-    
-
 
     from .views import views
     from .auth import auth
@@ -20,15 +18,16 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User, Note
+    from .models import User, Messages
 
-    #create_database(app)
+    # create_database(app)
 
     with app.app_context():
         db.create_all()
 
     login_manager = LoginManager()
-    login_manager.login_view = 'views.intro' #where to redirect if user is not logged in
+    # where to redirect if user is not logged in
+    login_manager.login_view = 'views.intro'
     login_manager.init_app(app)
 
     @login_manager.user_loader
@@ -37,7 +36,9 @@ def create_app():
 
     return app
 
-#if databse does not exist then create database
+# if databse does not exist then create database
+
+
 def create_database(app):
     if not path.exists('website/' + DB_NAME):
         db.create_all(app=app)
--- website/models.py
+++ website/models.py
@@ -2,16 +2,21 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-class Note(db.Model):
+
+class Messages(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now()) #func.now gets current date/time
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) #stores id of user who posted note
+    # func.now gets current date/time
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    # stores id of user who posted Messages
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-#Below is schema for user database
+# Below is schema for user database
+
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    notes = db.relationship('Note')
+    Messages = db.relationship('Messages')
