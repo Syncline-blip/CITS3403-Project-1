5d24d419d2bf66cec18853b1def77751689c2c52
johnald1337 <johnald1337@hotmail.com>
2023-04-05 16:10:18 +0800

    Added extra links to navbar

--- website/views.py
+++ website/views.py
@@ -15,7 +15,6 @@ def home():
     return render_template("home.html", user=current_user)
 
 
-
 @views.route('/chat_room_1')
 @login_required  # makes this page accessible only if user is logged in
 def chat_room_1():
@@ -32,3 +31,24 @@ def chat_room_2():
 @login_required  # makes this page accessible only if user is logged in
 def chat_room_3():
     return render_template("chat_room_3.html", user=current_user)
+
+@views.route('/about_us')
+@login_required  # makes this page accessible only if user is logged in
+def about_us():
+    return render_template("about_us.html", user=current_user)
+
+@views.route('/user_terms')
+@login_required  # makes this page accessible only if user is logged in
+def user_terms():
+    return render_template("user_terms.html", user=current_user)
+
+@views.route('/privacy')
+@login_required  # makes this page accessible only if user is logged in
+def privacy():
+    return render_template("privacy.html", user=current_user)
+
+
+@views.route('/contact_us')
+@login_required  # makes this page accessible only if user is logged in
+def contact_us():
+    return render_template("contact_us.html", user=current_user)
