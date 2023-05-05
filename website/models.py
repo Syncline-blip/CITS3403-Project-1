fbc5f7407e3b441a80bba281c38b3692b2e94738
johnald1337 <johnald1337@hotmail.com>
2023-05-05 16:56:19 +0800

    model update with RoomMembers

--- website/models.py
+++ website/models.py
@@ -11,7 +11,15 @@ class Room(db.Model):
     description = db.Column(db.String(255))
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     messages = db.relationship('Messages', backref='room', lazy=True)
+    # Define a ManyToMany relationship between Room and User models
+    members = db.relationship('User', secondary='room_members',
+                              backref=db.backref('rooms', lazy=True))
 
+
+class RoomMembers(db.Model):
+    __tablename__ = 'room_members'
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    room_id = db.Column(db.Integer, db.ForeignKey('room.id'), primary_key=True)
 
 class Members(db.Model):
     member = db.Column(db.Integer, primary_key=True)
