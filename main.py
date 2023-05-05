7f013d2210068ece2a2e704e8522e49649ad41a1
johnald1337 <johnald1337@hotmail.com>
2023-05-05 18:22:37 +0800

    removed uneeded code in main

--- main.py
+++ main.py
@@ -3,12 +3,5 @@ from website.sockets import connect, disconnect, message
 
 app = create_app()
 
-
-# I dont think we need below but leaving just incase for now
-'''socketio.on_event("connect", connect)
-socketio.on_event("disconnect", disconnect)
-socketio.on_event("new-message", message)'''
-
-
 if __name__ == '__main__':
     socketio.run(app,debug=True) 
