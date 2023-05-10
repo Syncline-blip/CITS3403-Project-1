18d1cb57f5b203476e0875041bd7874648d62538
johnald1337 <johnald1337@hotmail.com>
2023-05-10 16:12:07 +0800

    right navbar appers when logged out

--- website/views.py
+++ website/views.py
@@ -9,21 +9,17 @@ def intro():
     return render_template("intro.html", user=current_user)
 
 @views.route('/about_us')
-@login_required  # makes this page accessible only if user is logged in
 def about_us():
     return render_template("about_us.html", user=current_user)
 
 @views.route('/user_terms')
-@login_required  # makes this page accessible only if user is logged in
 def user_terms():
     return render_template("user_terms.html", user=current_user)
 
 @views.route('/privacy')
-@login_required  # makes this page accessible only if user is logged in
 def privacy():
     return render_template("privacy.html", user=current_user)
 
 @views.route('/contact_us')
-@login_required  # makes this page accessible only if user is logged in
 def contact_us():
     return render_template("contact_us.html", user=current_user)
