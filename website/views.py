10a19fed2fcfc923390909f73b0317f2e4600c8f
johnald1337 <johnald1337@hotmail.com>
2023-05-16 22:28:19 +0800

    slight tune up

--- website/views.py
+++ website/views.py
@@ -3,14 +3,13 @@ from flask_login import current_user
 
 views = Blueprint('views', __name__)
 
-
 #Injects the current user into the template context.
 @views.app_context_processor
 def inject_user():
     return dict(user=current_user)
 
-# Define the route for the application root ('/') 
-# If the current user is authenticated, they are redirected to the home page. If not, the intro page is displayed.
+#Define the route for the application root ('/') 
+#If the current user is authenticated, they are redirected to the home page. If not, the intro page is displayed.
 @views.route('/')
 def intro():
     if current_user.is_authenticated:
@@ -18,17 +17,17 @@ def intro():
     else:
         return render_template("intro.html")
 
-# Define the route for the about_us page. 
+#Define the route for the about_us page. 
 @views.route('/about_us')
 def about_us():
     return render_template("about_us.html")
 
-# Define the route for the privacy page.
+#Define the route for the privacy page.
 @views.route('/privacy')
 def privacy():
     return render_template("privacy.html")
 
-# Define the route for the contact_us page.
+#Define the route for the contact_us page.
 @views.route('/contact_us')
 def contact_us():
     return render_template("contact_us.html")
