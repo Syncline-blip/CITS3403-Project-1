9b99d68243f241c28d300ad328a29ec29a7ad38f
johnald1337 <johnald1337@hotmail.com>
2023-05-16 22:24:41 +0800

    views.py clean/comments

--- website/views.py
+++ website/views.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, redirect, render_template, url_for
-from flask_login import login_required, current_user
+from flask_login import current_user
 
 views = Blueprint('views', __name__)
 
 
+# Define the route for the application root ('/') 
+# If the current user is authenticated, they are redirected to the home page. If not, the intro page is displayed.
 @views.route('/')
 def intro():
     if current_user.is_authenticated:
@@ -11,14 +13,17 @@ def intro():
     else:
         return render_template("intro.html", user=current_user)
 
+# Define the route for the about_us page. 
 @views.route('/about_us')
 def about_us():
     return render_template("about_us.html", user=current_user)
 
+# Define the route for the privacy page.
 @views.route('/privacy')
 def privacy():
     return render_template("privacy.html", user=current_user)
 
+# Define the route for the contact_us page.
 @views.route('/contact_us')
 def contact_us():
     return render_template("contact_us.html", user=current_user)
