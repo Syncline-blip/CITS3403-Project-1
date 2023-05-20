627e915780860f26488847746b86785aaf814a7a
johnald1337 <johnald1337@hotmail.com>
2023-05-20 22:45:53 +0800

    clean/commets sockets.py

--- website/sockets.py
+++ website/sockets.py
@@ -1,54 +1,65 @@
 from flask_socketio import join_room, leave_room, send, emit
 from flask import session
-
-from website.game_code import computer_message, handle_hangman, handle_normal_mode, handle_scramble_mode, hangman_stop, scramble_timer_done, start_scramble, startHangman
+from website.game_code import ( 
+    computer_message, 
+    handle_hangman, 
+    handle_normal_mode, 
+    handle_scramble_mode, 
+    hangman_stop, 
+    scramble_timer_done, 
+    start_scramble, 
+    startHangman
+)
 from . import db, socketio
-from .models import Messages, Room, User, ActiveMembers
+from .models import Room, User, ActiveMembers
 from flask_login import current_user
 from datetime import datetime
 import re
-import random
-import string
+
 
 DATE_FORMAT = "%H:%M:%S %d-%m-%Y"
-
 general_rooms = ["GLOB", "LFGG", "SUPP"]
 
-global word_string
 
-
-
-
+# Socket.io event listener for when a client connects to the server
 @socketio.on("connect")
 def connect():
-    
+    # Retrieve room and username from session
     room = session.get("room")
     username = session.get("username") 
     if not room or not username:
         return
+
+    # Query to find the room object with the same name
     room_obj = Room.query.filter_by(room_name=room).first()
     if not room_obj:
+        # If room doesn't exist, leave room and return
         leave_room(room)
         return
-    if room_obj.game_mode == 1:
-        gameMode = "ON"
-    else:
-        gameMode = "OFF"
+
+    # Check the room's game mode
+    gameMode = "ON" if room_obj.game_mode == 1 else "OFF"
     
+    # Query to find the user object with the same username
     user_obj = User.query.filter_by(username=username).first()
     profile_picture = user_obj.profile_picture if user_obj else None
+
+    # Get all active members in the room
     all_members = ActiveMembers.query.filter_by(room_id=room_obj.id).all()
     member_list = [x.user_id for x in all_members] #gets all users in the room atm
     username_list = []
     profile_list = []
 
+    # Retrieve the username and profile picture of all active members
     for person in member_list:
         relevant_person = User.query.filter_by(id=person).first()
         username_list.append(relevant_person.username)
         profile_list.append(relevant_person.profile_picture)
 
-
+    # Get current date and time
     date = datetime.now()
+
+    # Create content dictionary
     content = {
         "username": username,
         "profile_picture": profile_picture,
@@ -58,11 +69,13 @@ def connect():
         "all_member_profiles": profile_list,
         "gameMode" : gameMode
     }
-    
+
+    # Create a new active member entry in the database
     new_member = ActiveMembers(user_id=current_user.id, room_id=room_obj.id)
     db.session.add(new_member)
     db.session.commit()
 
+    # Join the room and send the content to the room
     join_room(room)
     send(content, to=room) #Sends a message - handled in room.html scripts.
     print(f"{username} joined room {room}")
@@ -70,51 +83,66 @@ def connect():
 
 @socketio.on("disconnect")
 def disconnect():
+    # Get room and username from session
     room = session.get("room")
     username = session.get("username")
+    
+    # Get user object and profile picture
     user_obj = User.query.filter_by(username=username).first()
     profile_picture = user_obj.profile_picture if user_obj else None
+    
+    # Get current date and time
     date = datetime.now()
+    
+    # Create content dictionary for sending the message
     content = {
         "username": username,
         "profile_picture": profile_picture,
         "message": "has left the room",
         "date": date.strftime(DATE_FORMAT),
         "disconnecting": "true"
-        }
+    }
+    
+    # Leave the room
     leave_room(room)
 
+    # Remove the user from the active members
     room_obj = Room.query.filter_by(room_name=room).first() 
     ActiveMembers.query.filter_by(user_id=current_user.id, room_id=room_obj.id).delete()
     db.session.commit()
 
-    #If room is in game mode and everyone leaves the game ends itself 
+    # If room is in game mode and everyone leaves, the game ends itself
     active_members_count = ActiveMembers.query.filter_by(room_id=room_obj.id).count()
     if active_members_count == 0 and room_obj.game_mode != None:
         room_obj.game_mode = None
         room_obj.game_round = None
         room_obj.game_answer = None
         db.session.commit()
-        computer_message(room,"No Active Users, Game Over")
+        computer_message(room, "No Active Users, Game Over")
         
-    
-
+    # Send the content to the room
     send(content, to=room)
     print(f"{username} has left the room {room}")
 
-
-
 @socketio.on("new-message")
 def message(data):
+    # Get room from session
     room = session.get("room")
+    
+    # Get room object
     room_obj = Room.query.filter_by(room_name=room).first()
+    
     if not room_obj:
         return
 
+    # Get user object and profile picture
     user_obj = User.query.filter_by(username=session.get("username")).first()
     profile_picture = user_obj.profile_picture if user_obj else None
+    
+    # Get current date and time
     date = datetime.now().strftime(DATE_FORMAT)
 
+    # Create content dictionary for sending the message
     content = {
         "username": session.get("username"),
         "profile_picture": profile_picture,
@@ -122,17 +150,21 @@ def message(data):
         "date": date
     }
 
+    # Get the count of active members in the room
     active_members_count = ActiveMembers.query.filter_by(room_id=room_obj.id).count()
     print(f"Members: {active_members_count}")
-    #if the message is the below command, and not one of the 3 general rooms or private room, a word scramble game starts
+    
+    # Check if the message is a command for starting a word scramble game
     bad_scramble_call = re.search(r'\./scramble\b', data["data"])
     scramble_command = re.search(r'\./scramble\s+(\w+)$', data["data"])
     hangman_command = re.search(r'\./hangman\b', data["data"])
     print(hangman_command)
+    
     if room_obj.game_mode is None and len(room_obj.room_name) == 4 and room_obj.room_name not in general_rooms:
         if scramble_command:
-            #Game can only start when more then 1 person in the chat room
+            # Game can only start when more than 1 person in the chat room
             word = scramble_command.group(1)
+            
             '''if active_members_count == 1:
                 computer_message(room,"Not enough members to start a game")'''
             
@@ -154,16 +186,15 @@ def message(data):
                 start_scramble(room, room_obj, mode)
                 return
             else:
-                computer_message(room,f"Scramble Category '{word}' is invalid.")
+                computer_message(room, f"Scramble Category '{word}' is invalid.")
             
         elif hangman_command:
             mode = 10
             startHangman(room, room_obj, mode)
         elif bad_scramble_call:
-            computer_message(room,"Scramble call requires a category. Example: './scramble fruit'. Categories are: fruit, videogames and css")
-
+            computer_message(room, "Scramble call requires a category. Example: './scramble fruit'. Categories are: fruit, videogames and css")
         
-    #if game_mode == 1 it means a word scramble game is being played
+    # If game_mode == 1 it means a word scramble game is being played
     if room_obj.game_mode in [1, 2, 3]:
         handle_scramble_mode(room_obj, data["data"], content, room)
     elif room_obj.game_mode == 10:
@@ -171,14 +202,14 @@ def message(data):
     else:
         handle_normal_mode(room_obj, data["data"], content, room)
 
-
-
-
-
 @socketio.on("timer-done")
 def scramble_stop():
+    # Get room from session
     room = session.get("room")
+    
+    # Get room object
     room_obj = Room.query.filter_by(room_name=room).first()
+    
     if room_obj.game_mode != None:
         if room_obj.game_mode < 10:
             scramble_timer_done(room, room_obj)
