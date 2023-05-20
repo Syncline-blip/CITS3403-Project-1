6038076e6bddb0a994810a5bc49e56be066a4ee8
J Lumagbas <jadlumagbas@gmail.com>
2023-05-20 21:47:51 +0800

    Selenium able to go to chats, but still fails

--- selenium-testing/check_global_chat.py
+++ selenium-testing/check_global_chat.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import NoSuchElementException
 
 def test_user_interaction():
     driver = webdriver.Chrome('C:/Users/John Lumagbas/Desktop/GITHUB-UWA/CITS3403-Project-1/selenium-testing/chromedriver.exe')
@@ -21,13 +22,18 @@ def test_user_interaction():
     # Wait for the login process to complete
     driver.implicitly_wait(5)
 
-  
-
-
     # Enter the global chat
-    global_chat_link = driver.find_element("link text", "Global Chat")
-    global_chat_link.click()
+    global_chat_button = driver.find_element("name", "globalChat")
+    global_chat_button.click()
 
+    try:
+        chat_title = driver.find_element("class name", "chatTitle")
+        if chat_title.text == "Global Chat":
+            print("User successfully entered the Global Chat.")
+        else:
+            print("Failed to enter the Global Chat.")
+    except Exception:
+        print("Failed to find the Global Chat button.")
     # Wait for the global chat to load
     driver.implicitly_wait(5)
 
