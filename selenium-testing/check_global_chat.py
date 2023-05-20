07300a7f7fc1b9096ba315d662e405260be3e6a4
J Lumagbas <jadlumagbas@gmail.com>
2023-05-20 22:17:47 +0800

    passes and does the test, but output fails

--- selenium-testing/check_global_chat.py
+++ selenium-testing/check_global_chat.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException
 
 def test_user_interaction():
     driver = webdriver.Chrome('C:/Users/John Lumagbas/Desktop/GITHUB-UWA/CITS3403-Project-1/selenium-testing/chromedriver.exe')
@@ -12,30 +11,35 @@ def test_user_interaction():
 
     email_input = driver.find_element("id", "email")
     email_input.send_keys(email)
+    print("Email entered")
 
     password_input = driver.find_element("id", "password")
     password_input.send_keys(password)
+    print("Password entered")
 
     submit_button = driver.find_element("xpath", "//button[@type='submit']")
     submit_button.click()
+    print("Submit button clicked")
 
     # Wait for the login process to complete
     driver.implicitly_wait(5)
 
     # Enter the global chat
-    global_chat_button = driver.find_element("name", "globalChat")
-    global_chat_button.click()
-
     try:
+        global_chat_button = driver.find_element("css selector", ".chatLink[name='globalChat']")
+        global_chat_button.click()
+        print("Global Chat button clicked")
+        
+        # Wait for the global chat to load
+        driver.implicitly_wait(5)
+
         chat_title = driver.find_element("class name", "chatTitle")
         if chat_title.text == "Global Chat":
             print("User successfully entered the Global Chat.")
         else:
             print("Failed to enter the Global Chat.")
-    except Exception:
+    except NoSuchElementException:
         print("Failed to find the Global Chat button.")
-    # Wait for the global chat to load
-    driver.implicitly_wait(5)
 
     # Perform additional interactions or assertions as needed
 
